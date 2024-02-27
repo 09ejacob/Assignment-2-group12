d1e81bc63b329395478d2b3f6f42821cb39bbb77
Elias Jacobsen <eliasgj@stud.ntnu.no>
2024-02-27 11:45:00 +0100

    MovieTicketServer changes

---
+++ assignment2/src/main/java/org/example/MovieTicketServer.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class MovieTicketServer {
+    public String movieTitle;
+    public int availableSeats;
+
+    public MovieTicketServer(String movieTitle, int availableSeats) {
+        this.movieTitle = movieTitle;
+        this.availableSeats = availableSeats;
+    }
+
+    public void bookTicket(String customerName, int numberOfSeats) {
+        System.out.println("Hi, " + customerName + ". There are: " +
+                            this.availableSeats + " seats available for " +
+                            this.movieTitle);
+
+        if ((this.availableSeats - numberOfSeats) < 0) {
+            System.out.println("Hi, " + customerName + ". There are not enough available seats for " +
+                                this.movieTitle);
+            return;
+        }
+
+        this.availableSeats -= numberOfSeats;
+
+        System.out.println("Hi, " + customerName + ". There was: " + numberOfSeats +
+                            "successfully booked for " + this.movieTitle);
+    }
+}
